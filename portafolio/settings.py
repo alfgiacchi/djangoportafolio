fd5f238609bc11d6c1edf124b92b0c16e18968dd
alfgiacchi <agiacchi@gmail.com>
2024-10-15 20:00:26 -0300

    Update settings.py

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6mz83zxgtbom#40l1b877ri4w+&#bh7kgj8c1fnt2l24_!&+by'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
